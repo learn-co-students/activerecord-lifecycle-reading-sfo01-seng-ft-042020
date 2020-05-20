f5c9b51a227e7bf7b66d14ed83ffd173a7b835e3
github username <you@example.com>
2020-05-19 21:14:51 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+
+  before_save :email_author_about_post
+
   private
 
   def is_title_case
@@ -11,7 +15,13 @@ class Post < ActiveRecord::Base
     end
   end
 
+  def email_author_about_post
+    # Not implemented.
+    # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
+  end
+
   def make_title_case
+    # Rails provides a #titlecase method
     self.title = self.title.titlecase
   end
 end
